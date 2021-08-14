ef27b885c71f1b213d0ec9b70950ac9c225ed841
alex <alex3889660@gmail.com>
2021-08-14 11:01:52 +0800

    two-sum

--- 0001-two-sum.js
+++ 0001-two-sum.js
@@ -1,1 +1,17 @@
 // test for use ssh  test
+
+let twoSum = function (nums, target) {
+  console.log(nums);
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = i; j <= nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        console.log(`Output: [index[${i}] , index[${j}]], target = ${target}`);
+      }
+    }
+  }
+};
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let add = 8;
+
+twoSum(arr, add);
