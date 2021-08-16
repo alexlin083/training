37e7e78cfdbb6ac2fb3c895834600cd6be5a256e
alex <alex3889660@gmail.com>
2021-08-16 10:50:31 +0800

    O(n) & O(n^2)

--- 0001-two-sum.js
+++ 0001-two-sum.js
@@ -1,17 +1,36 @@
 // test for use ssh  test
-
-let twoSum = function (nums, target) {
-  console.log(nums);
+// O(n^2) 作法
+let twoSum1 = function (nums, target) {
+  //   console.log(nums);
   for (let i = 0; i <= nums.length; i++) {
-    for (let j = i; j <= nums.length; j++) {
+    for (let j = i + 1; j <= nums.length; j++) {
       if (nums[i] + nums[j] === target) {
-        console.log(`Output: [index[${i}] , index[${j}]], target = ${target}`);
+        console.log([i, j]);
+        return [i, j];
       }
     }
   }
 };
-
+const map = new Map();
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let add = 8;
+let add = 13;
 
-twoSum(arr, add);
+twoSum1(arr, add);
+// console.log(i, j);
+//--------------------------------------------------------------------------------
+// // Map
+let twoSum2 = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let value = nums[i];
+    if (map[target - value] >= 0) {
+      console.log([map[target - value], i]);
+
+      return [map[target - value], i];
+    } else {
+      map[value] = i;
+    }
+  }
+};
+twoSum2(arr, add);
+// // console.log(map);
