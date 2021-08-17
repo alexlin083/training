cf68bd5bd8326cb2cefecc87849fd48bf695f78b
alex <alex3889660@gmail.com>
2021-08-17 22:10:51 +0800

    add 20

--- 0020-valid-parentheses.js
+++ 0020-valid-parentheses.js
@@ -1,51 +1,95 @@
-let isValid = function () {
-  let arr = Array.from("(){[()]}[]");
+let isValid = function (arr) {
   let stack = [];
   console.log(arr);
 
-  //先判斷不是右邊括號
-  if (
-    arr[0].charCodeAt() !== 41 ||
-    arr[0].charCodeAt() !== 93 ||
-    arr[0].charCodeAt() !== 125
-  ) {
+  //
+  if (arr.length > 1) {
     for (let i = 0; i < arr.length; i++) {
       if (
-        arr[i].charCodeAt() == 40 ||
-        arr[i].charCodeAt() == 91 ||
-        arr[i].charCodeAt() == 123
+        arr[i].charCodeAt() === 40 ||
+        arr[i].charCodeAt() === 91 ||
+        arr[i].charCodeAt() === 123
       ) {
         stack.push(arr[i]);
         // console.log("stack :", stack);
         // console.log("stack[-1] :", stack[stack.length - 1]);
       } else if (
-        arr[i].charCodeAt() == 41 ||
-        arr[i].charCodeAt() == 93 ||
-        arr[i].charCodeAt() == 125
+        arr[i].charCodeAt() === 41 ||
+        arr[i].charCodeAt() === 93 ||
+        arr[i].charCodeAt() === 125
       ) {
         if (
-          arr[i].charCodeAt() - 1 == arr[i - 1].charCodeAt() ||
-          arr[i].charCodeAt() - 2 == arr[i - 1].charCodeAt() ||
-          arr[i].charCodeAt() - 1 == stack[stack.length - 1].charCodeAt() ||
-          arr[i].charCodeAt() - 2 == stack[stack.length - 1].charCodeAt()
+          // arr[i].charCodeAt() - 1 === arr[i - 1].charCodeAt() ||
+          // arr[i].charCodeAt() - 2 === arr[i - 1].charCodeAt() ||
+          arr[i].charCodeAt() - 1 === stack[stack.length - 1].charCodeAt() ||
+          arr[i].charCodeAt() - 2 === stack[stack.length - 1].charCodeAt()
         ) {
           //   console.log(stack);
           stack.pop(-1);
-          //   console.log(stack);
-          console.log("true");
+          // stack.push(arr[i]);
+          console.log(stack);
+          // console.log("true");
+          // return true;
         } else {
           console.log("false");
+          return false;
         }
       }
+
       //   return arr[i];
     }
-    // console.log(stack);
+    console.log("true");
+
+    //   console.log(stack.join(""));
+    return true;
   } else {
     console.log("false");
+    return false;
   }
+  //----------------------------------------------------------------------更新前
+  //先判斷不是右邊括號
+  //   if (
+  //     arr[0].charCodeAt() !== 41 ||
+  //     arr[0].charCodeAt() !== 93 ||
+  //     arr[0].charCodeAt() !== 125
+  //   ) {
+  //     for (let i = 0; i < arr.length; i++) {
+  //       if (
+  //         arr[i].charCodeAt() == 40 ||
+  //         arr[i].charCodeAt() == 91 ||
+  //         arr[i].charCodeAt() == 123
+  //       ) {
+  //         stack.push(arr[i]);
+  //         // console.log("stack :", stack);
+  //         // console.log("stack[-1] :", stack[stack.length - 1]);
+  //       } else if (
+  //         arr[i].charCodeAt() == 41 ||
+  //         arr[i].charCodeAt() == 93 ||
+  //         arr[i].charCodeAt() == 125
+  //       ) {
+  //         if (
+  //           arr[i].charCodeAt() - 1 == arr[i - 1].charCodeAt() ||
+  //           arr[i].charCodeAt() - 2 == arr[i - 1].charCodeAt() ||
+  //           arr[i].charCodeAt() - 1 == stack[stack.length - 1].charCodeAt() ||
+  //           arr[i].charCodeAt() - 2 == stack[stack.length - 1].charCodeAt()
+  //         ) {
+  //           //   console.log(stack);
+  //           stack.pop(-1);
+  //           //   console.log(stack);
+  //           console.log("true");
+  //         } else {
+  //           console.log("false");
+  //         }
+  //       }
+  //       //   return arr[i];
+  //     }
+  //     // console.log(stack);
+  //   } else {
+  //     console.log("false");
+  //   }
 };
 
-isValid();
+isValid("((");
 
 /* ASCII
 ( = 40
